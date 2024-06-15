89888fbabce542dabb3f765aad718d2ce24b7aff
NitichaiAom <NitichaiSawangsai@gmail.com>
2024-06-15 14:34:10 +0700

    fix: fixed update code

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'ctv94z',
   e2e: {
     baseUrl: 'https://example.cypress.io',
     setupNodeEvents(on, config) {
